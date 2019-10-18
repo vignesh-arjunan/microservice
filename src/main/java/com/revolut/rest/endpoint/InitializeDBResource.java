1c234ad1fd4375590646c9c55f7c8ed207aca068
vignesh_arjunan <vignesh.arjunan@gmail.com>
2019-10-18 07:32:43 +0530

    added tests

--- src/main/java/com/revolut/rest/endpoint/InitializeDBResource.java
+++ src/main/java/com/revolut/rest/endpoint/InitializeDBResource.java
@@ -1,8 +1,8 @@
 package com.revolut.rest.endpoint;
 
 import com.revolut.db.DbOperation;
+import com.revolut.pojo.Message;
 import lombok.extern.java.Log;
-import org.jooq.h2.generated.tables.records.TransfersRecord;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -11,12 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
-import static org.jooq.h2.generated.Tables.TRANSFERS;
 
 @RequestScoped
 @Path("/db/initialize")
@@ -28,7 +22,7 @@ public class InitializeDBResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response initDB() throws SQLException {
+    public Response initDB() {
         try (var con = dbOperation.getConnection(); var stm = con.createStatement()) {
             stm.execute("CREATE TABLE accounts(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), balance NUMERIC)");
             stm.execute("INSERT INTO accounts(name, balance) VALUES('Jack', 52642.5)");
@@ -37,6 +31,10 @@ public class InitializeDBResource {
             stm.execute("INSERT INTO accounts(name, balance) VALUES('Vada', 29000)");
 
             stm.execute("CREATE TABLE transfers(id BIGINT PRIMARY KEY AUTO_INCREMENT, from_account BIGINT, to_account BIGINT, amount NUMERIC, at TIMESTAMP, comment VARCHAR(255))");
+        } catch (Exception ex) {
+            return Response.ok(
+                    new Message("DB already created", null)
+            ).build();
         }
 
         return Response.ok(
