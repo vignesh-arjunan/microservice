9ef8dca5454648ed1a347a17fa43613e54108c92
Vignesh Arjunan <vignesh.arjunan@gmail.com>
2020-07-13 20:17:50 +0530

    added multipart upload

--- src/main/java/com/micro/rest/endpoint/fileupload/Upload.java
+++ src/main/java/com/micro/rest/endpoint/fileupload/Upload.java
@@ -3,10 +3,7 @@ package com.micro.rest.endpoint.fileupload;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.FileOutputStream;
@@ -27,5 +24,16 @@ public class Upload {
             outputStream.flush();
         }
         return Response.ok().build();
+    }
+
+    @POST
+    @Path("/multipart")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Encoded
+    public Response upload(@FormParam("file") String request,
+                           @FormParam("string") String string ) throws IOException {
+        System.out.println(string);
+        return Response.ok(request).build();
     }
 }
